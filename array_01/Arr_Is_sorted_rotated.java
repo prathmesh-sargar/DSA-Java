6d3a8470703443ad19bcb9fa41dbe0003426f9da
prathmeshTech07005 <sargarprathmesh007@gmail.com>
2025-06-10 13:11:27 +0530

    last questions is added

--- array_01/Arr_Is_sorted_rotated.java
+++ array_01/Arr_Is_sorted_rotated.java
@@ -3,10 +3,31 @@
 // Input: [3, 4, 5, 1, 2]  
 // Output: true
 
+//  Check if array is sorted and rotated
 package array_01;
 
 public class Arr_Is_sorted_rotated {
+
     public static void main(String[] args) {
+
+        // given array is 
+        int arr[] = {3, 4, 5, 1,2};
+
+        // base condition 
+        if(arr[0]>arr[1]){
+            System.err.println("true");
+        }
+        if(arr[arr.length-1] > arr[arr.length-2]){
+            System.out.println("true");
+        }
+        // Given an array, check if it is sorted in increasing order and then rotated.
+        for(int i=1;i<arr.length-1;i++){
+            if(arr[i] < arr[i-1] && arr[i] < arr[i+1]){
+                 System.err.println("true");
+                 break;
+            }
+        }
+
         
     }
 }
